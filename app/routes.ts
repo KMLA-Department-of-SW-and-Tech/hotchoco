82eec21853afdcc1f8b642a94b3e3fe1ebd756ab
survibo <survibo@gmail.com>
2025-10-24 09:08:34 +0900

    Change URL routes
    searchEasy로 main code index를 전환함

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,3 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/search.jsx"), route("/easy", "routes/searchEasy.jsx")] satisfies RouteConfig;
+export default [index("routes/searchEasy.jsx"), route("/original", "routes/search.jsx")] satisfies RouteConfig;
